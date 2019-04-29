e194392ac517e55a1bc50b234d16984b905a7f7e
MajlindaJaha <44609607+MajlindaJaha@users.noreply.github.com>
2019-04-29 22:43:13 +0200

    Dekriptimi per tekst

--- TripleDES/Form1.Designer.cs
+++ TripleDES/Form1.Designer.cs
@@ -29,18 +29,23 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.Button btnEnkripto;
+            System.Windows.Forms.Button btnDekripto;
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             btnEnkripto = new System.Windows.Forms.Button();
+            btnDekripto = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnEnkripto
             // 
-            btnEnkripto.Location = new System.Drawing.Point(116, 96);
+            btnEnkripto.Location = new System.Drawing.Point(87, 78);
+            btnEnkripto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             btnEnkripto.Name = "btnEnkripto";
-            btnEnkripto.Size = new System.Drawing.Size(145, 48);
+            btnEnkripto.Size = new System.Drawing.Size(109, 39);
             btnEnkripto.TabIndex = 0;
             btnEnkripto.Text = "Enkripto";
             btnEnkripto.UseVisualStyleBackColor = true;
@@ -49,45 +54,82 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 47);
+            this.label1.Location = new System.Drawing.Point(24, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Teksti";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(116, 42);
+            this.textBox1.Location = new System.Drawing.Point(87, 34);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(393, 22);
+            this.textBox1.Size = new System.Drawing.Size(296, 20);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(116, 171);
+            this.textBox2.Location = new System.Drawing.Point(87, 139);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(393, 22);
+            this.textBox2.Size = new System.Drawing.Size(296, 20);
             this.textBox2.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 176);
+            this.label2.Location = new System.Drawing.Point(24, 143);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Enkriptimi";
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(87, 240);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(296, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 244);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Dekriptimi";
+            // 
+            // btnDekripto
+            // 
+            btnDekripto.Location = new System.Drawing.Point(87, 179);
+            btnDekripto.Margin = new System.Windows.Forms.Padding(2);
+            btnDekripto.Name = "btnDekripto";
+            btnDekripto.Size = new System.Drawing.Size(109, 39);
+            btnDekripto.TabIndex = 6;
+            btnDekripto.Text = "Dekripto";
+            btnDekripto.UseVisualStyleBackColor = true;
+            btnDekripto.Click += new System.EventHandler(this.btnDekripto_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(524, 380);
+            this.ClientSize = new System.Drawing.Size(393, 309);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(btnDekripto);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(btnEnkripto);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -100,6 +142,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- TripleDES/Form1.cs
+++ TripleDES/Form1.cs
@@ -37,5 +37,24 @@ namespace TripleDES
             textBox2.Text = Convert.ToBase64String(rezultati);
 
         }
+
+        private void btnDekripto_Click(object sender, EventArgs e)
+        {
+            string key = "Projekti Ne Siguri";
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] keyArr = md5.ComputeHash(Encoding.UTF8.GetBytes(key));
+            md5.Clear();
+
+            byte[] EnkriptimiTxt = Convert.FromBase64String(textBox2.Text);
+
+            TripleDESCryptoServiceProvider des3 = new TripleDESCryptoServiceProvider();
+            des3.Key = keyArr;
+            des3.Mode = CipherMode.ECB;
+            des3.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform iTransform = des3.CreateDecryptor();
+            byte[] rezultati = iTransform.TransformFinalBlock(EnkriptimiTxt, 0, EnkriptimiTxt.Length);
+            textBox3.Text = Encoding.UTF8.GetString(rezultati);
+        }
     }
 }
