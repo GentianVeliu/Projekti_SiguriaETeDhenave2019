94894e43ace4dd364b54551a0c1d1f59ab6b25b1
GentianVeliu <44606115+GentianVeliu@users.noreply.github.com>
2019-05-01 11:24:13 +0200

    gabim

--- TripleDES/Form1.Designer.cs
+++ TripleDES/Form1.Designer.cs
@@ -36,143 +36,91 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.lblZgjedhFajllin = new System.Windows.Forms.Label();
-            this.txtFajlli = new System.Windows.Forms.TextBox();
-            this.btnZgjedh = new System.Windows.Forms.Button();
-            this.btnEnkript = new System.Windows.Forms.Button();
             btnEnkripto = new System.Windows.Forms.Button();
             btnDekripto = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnEnkripto
             // 
-            btnEnkripto.Location = new System.Drawing.Point(116, 96);
-            btnEnkripto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            btnEnkripto.Location = new System.Drawing.Point(87, 78);
+            btnEnkripto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             btnEnkripto.Name = "btnEnkripto";
-            btnEnkripto.Size = new System.Drawing.Size(145, 48);
+            btnEnkripto.Size = new System.Drawing.Size(109, 39);
             btnEnkripto.TabIndex = 0;
             btnEnkripto.Text = "Enkripto";
             btnEnkripto.UseVisualStyleBackColor = true;
             btnEnkripto.Click += new System.EventHandler(this.btnEnkripto_Click);
             // 
-            // btnDekripto
-            // 
-            btnDekripto.Location = new System.Drawing.Point(116, 220);
-            btnDekripto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            btnDekripto.Name = "btnDekripto";
-            btnDekripto.Size = new System.Drawing.Size(145, 48);
-            btnDekripto.TabIndex = 6;
-            btnDekripto.Text = "Dekripto";
-            btnDekripto.UseVisualStyleBackColor = true;
-            btnDekripto.Click += new System.EventHandler(this.btnDekripto_Click);
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 47);
+            this.label1.Location = new System.Drawing.Point(24, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Teksti";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(116, 42);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Location = new System.Drawing.Point(87, 34);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(393, 22);
+            this.textBox1.Size = new System.Drawing.Size(296, 20);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(116, 171);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Location = new System.Drawing.Point(87, 139);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(393, 22);
+            this.textBox2.Size = new System.Drawing.Size(296, 20);
             this.textBox2.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 176);
+            this.label2.Location = new System.Drawing.Point(24, 143);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Enkriptimi";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(116, 295);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Location = new System.Drawing.Point(87, 240);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(393, 22);
+            this.textBox3.Size = new System.Drawing.Size(296, 20);
             this.textBox3.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(32, 300);
+            this.label3.Location = new System.Drawing.Point(24, 244);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Dekriptimi";
             // 
-            // lblZgjedhFajllin
+            // btnDekripto
             // 
-            this.lblZgjedhFajllin.AutoSize = true;
-            this.lblZgjedhFajllin.Location = new System.Drawing.Point(539, 21);
-            this.lblZgjedhFajllin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblZgjedhFajllin.Name = "lblZgjedhFajllin";
-            this.lblZgjedhFajllin.Size = new System.Drawing.Size(92, 17);
-            this.lblZgjedhFajllin.TabIndex = 32;
-            this.lblZgjedhFajllin.Text = "Zgjedh fajllin:";
-            // 
-            // txtFajlli
-            // 
-            this.txtFajlli.Location = new System.Drawing.Point(542, 42);
-            this.txtFajlli.Margin = new System.Windows.Forms.Padding(4);
-            this.txtFajlli.Name = "txtFajlli";
-            this.txtFajlli.ReadOnly = true;
-            this.txtFajlli.Size = new System.Drawing.Size(288, 22);
-            this.txtFajlli.TabIndex = 30;
-            // 
-            // btnZgjedh
-            // 
-            this.btnZgjedh.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnZgjedh.Location = new System.Drawing.Point(838, 42);
-            this.btnZgjedh.Margin = new System.Windows.Forms.Padding(4);
-            this.btnZgjedh.Name = "btnZgjedh";
-            this.btnZgjedh.Size = new System.Drawing.Size(87, 22);
-            this.btnZgjedh.TabIndex = 31;
-            this.btnZgjedh.Text = "...";
-            this.btnZgjedh.UseVisualStyleBackColor = true;
-            this.btnZgjedh.Click += new System.EventHandler(this.btnZgjedh_Click);
-            // 
-            // btnEnkript
-            // 
-            this.btnEnkript.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnEnkript.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnEnkript.Font = new System.Drawing.Font("Arial", 11.25F);
-            this.btnEnkript.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnEnkript.Location = new System.Drawing.Point(542, 220);
-            this.btnEnkript.Margin = new System.Windows.Forms.Padding(4);
-            this.btnEnkript.Name = "btnEnkript";
-            this.btnEnkript.Size = new System.Drawing.Size(131, 42);
-            this.btnEnkript.TabIndex = 33;
-            this.btnEnkript.Text = "EnkriptoFile";
-            this.btnEnkript.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnEnkript.UseVisualStyleBackColor = true;
-            this.btnEnkript.Click += new System.EventHandler(this.btnEnkript_Click);
+            btnDekripto.Location = new System.Drawing.Point(87, 179);
+            btnDekripto.Margin = new System.Windows.Forms.Padding(2);
+            btnDekripto.Name = "btnDekripto";
+            btnDekripto.Size = new System.Drawing.Size(109, 39);
+            btnDekripto.TabIndex = 6;
+            btnDekripto.Text = "Dekripto";
+            btnDekripto.UseVisualStyleBackColor = true;
+            btnDekripto.Click += new System.EventHandler(this.btnDekripto_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1073, 393);
-            this.Controls.Add(this.btnEnkript);
-            this.Controls.Add(this.lblZgjedhFajllin);
-            this.Controls.Add(this.txtFajlli);
-            this.Controls.Add(this.btnZgjedh);
+            this.ClientSize = new System.Drawing.Size(393, 309);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(btnDekripto);
@@ -181,7 +129,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(btnEnkripto);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -196,10 +144,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label lblZgjedhFajllin;
-        private System.Windows.Forms.TextBox txtFajlli;
-        private System.Windows.Forms.Button btnZgjedh;
-        private System.Windows.Forms.Button btnEnkript;
     }
 }
 
--- TripleDES/Form1.cs
+++ TripleDES/Form1.cs
@@ -8,7 +8,6 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
 namespace TripleDES
 {
@@ -56,51 +55,6 @@ namespace TripleDES
             ICryptoTransform iTransform = des3.CreateDecryptor();
             byte[] rezultati = iTransform.TransformFinalBlock(EnkriptimiTxt, 0, EnkriptimiTxt.Length);
             textBox3.Text = Encoding.UTF8.GetString(rezultati);
-        }
-
-        private void btnZgjedh_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog fd = new OpenFileDialog();
-            fd.Title = "Zgjedh Fajllin";
-            fd.Filter = "Text File|*.txt|Word Document|*.docx";
-            if (fd.ShowDialog() == DialogResult.OK)
-            {
-                txtFajlli.Text = fd.FileName;
-            }
-        }
-
-        private void btnEnkript_Click(object sender, EventArgs e)
-        {
-            UTF8Encoding utf8 = new UTF8Encoding();
-            TripleDESCryptoServiceProvider tDES = new TripleDESCryptoServiceProvider();
-            tDES.Key = utf8.GetBytes(txtkey.Text);  //me utf8 merr edhe disa karaktere tveqanta
-            tDES.Mode = CipherMode.CBC;
-            tDES.IV = utf8.GetBytes("06041995");
-            tDES.Padding = PaddingMode.Zeros;
-
-            StreamReader sr = new StreamReader(txtFajlli.Text);
-            string permbajtja = sr.ReadToEnd();
-            sr.Close();
-            FileStream fs1 = new FileStream(txtFajlli.Text, FileMode.Create, FileAccess.Write);
-            CryptoStream cs = new CryptoStream(fs1, tDES.CreateEncryptor(), CryptoStreamMode.Write);
-            StreamWriter sw = new StreamWriter(cs);
-            sw.Write(permbajtja);
-            sw.Flush();
-            sw.Close();
-            //Ruajtja e qelsit ne fajll
-            string permbajtjaQelsit = txtkey.Text;
-            SaveFileDialog saveFD = new SaveFileDialog();
-            saveFD.InitialDirectory = Convert.ToString(Environment.SpecialFolder.Desktop);
-            saveFD.Filter = "Text File|*.txt|Word Document|*.docx";
-            saveFD.FilterIndex = 1;
-            if (saveFD.ShowDialog() == DialogResult.OK && saveFD.FileName.Length > 0)
-            {
-                FileStream fs2 = new FileStream(saveFD.FileName, FileMode.Create, FileAccess.Write);
-                StreamWriter sw2 = new StreamWriter(fs2);
-                sw2.Write(permbajtjaQelsit);
-                sw2.Flush();
-                sw2.Close();
-            }
         }
     }
 }
